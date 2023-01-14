3acb29e83c77902e360ac6b2d1600c2399a6cd46
buianhtai1205 <buianhtai2017tq@gmail.com>
2023-01-14 18:59:54 +0700

    basic sidebar content

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,9 @@
 const images = {
 	logo: require("~/assets/images/logo.svg").default,
 	moonIcon: require("~/assets/images/moonIcon.svg").default,
+	homeIcon: require("~/assets/images/homeIcon.svg").default,
+	followingIcon: require("~/assets/images/followingIcon.svg").default,
+	liveIcon: require("~/assets/images/liveIcon.svg").default,
 };
 
 export { images };
--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,11 +1,79 @@
 import clsx from "clsx";
+import { useEffect, useState } from "react";
+import AccountItem from "~/components/AccountItem";
 import styles from "./Sidebar.module.scss";
+import { images } from "~/assets/images";
 
 const Sidebar = () => {
+	const [suggestedAccounts, setSuggestedAccounts] = useState([]);
+	useEffect(() => {
+		//fake api
+		const apiResult = [
+			{
+				username: "nguyenvana.offical",
+				fullname: "Nguyen Van A",
+				images: "https://img.meta.com.vn/Data/image/2022/01/06/avatar-tiktok-6.jpg",
+				isTick: true,
+			},
+			{
+				username: "nguyenvana.offical",
+				fullname: "Nguyen Van A",
+				images: "https://img.meta.com.vn/Data/image/2022/01/06/avatar-tiktok-6.jpg",
+				isTick: true,
+			},
+			{
+				username: "nguyenvana.offical",
+				fullname: "Nguyen Van A",
+				images: "https://img.meta.com.vn/Data/image/2022/01/06/avatar-tiktok-6.jpg",
+				isTick: false,
+			},
+			{
+				username: "nguyenvana.offical",
+				fullname: "Nguyen Van A",
+				images: "https://img.meta.com.vn/Data/image/2022/01/06/avatar-tiktok-6.jpg",
+				isTick: false,
+			},
+		];
+
+		setSuggestedAccounts(apiResult);
+	}, []);
 	return (
 		<aside className={clsx(styles.wrapper)}>
 			{/* Sidebar */}
-			<h2>Sidebar</h2>
+			<div className={clsx(styles.navBar, styles.container)}>
+				<div className={clsx(styles.navBarContent, styles.home)}>
+					<img src={images.homeIcon} alt="home" />
+					For You
+				</div>
+				<div className={clsx(styles.navBarContent)}>
+					<img src={images.followingIcon} alt="following" />
+					Following
+				</div>
+				<div className={clsx(styles.navBarContent)}>
+					<img src={images.liveIcon} alt="live" />
+					LIVE
+				</div>
+			</div>
+
+			<div className={clsx(styles.suggestedAccounts, styles.container)}>
+				<h4>Suggested accounts</h4>
+				{suggestedAccounts.map((item, index) => (
+					<AccountItem key={index} accountInfo={item} />
+				))}
+				<span>See all</span>
+			</div>
+
+			<div className={clsx(styles.discover, styles.container)}>
+				<h4>Discover</h4>
+			</div>
+
+			<div className={clsx(styles.footer)}>
+				<a href="/">About</a>
+				<a href="/">Newsrooms</a>
+				<a href="/">Contact</a>
+				<a href="/">Careers</a>
+				<a href="/">ByteDance</a>
+			</div>
 		</aside>
 	);
 };
